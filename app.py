51e76208c5d3f4794667851a1662ae1fe4ad9854
Gowreesh <gowreeshggopal@gmail.com>
2022-03-30 15:43:47 +0530

    second commit

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from flask import render_template
 from flask import request
 
-from flask import Flask, redirect, url_for, request, abort
+from flask import redirect, url_for, abort
 from markupsafe import escape
 
+
 app = Flask(__name__)
+
 
 @app.route('/welcome/')
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
 
 @app.route('/Gowreesh/')
 def gowreesh():
@@ -21,24 +23,27 @@ def gowreesh():
 def gowreesh_param(param):
     return '<h1>Welcome {}</h1>'.format(escape(param.capitalize()))
 
+
 @app.route('/add/<int:v1>/<int:v2>/')
-def gowreesh_add(v1,v2):
+def gowreesh_add(v1, v2):
     return '<h1>Answer:{}</h1>'.format(v1+v2)
+
 
 @app.route('/user/<int:user_id>/')
 def gowreesh_user(user_id):
-    users = ['G','O','W']
+    users = ['G', 'O', 'W']
     try:
         return '<h1>Welcome:{}</h1>'.format(users[user_id])
     except Exception:
         abort(404)
 
-@app.route('/login',methods = ['POST','GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         User = request.form['name']
-        return redirect(url_for('gowreesh', param = User))
-    
+        return redirect(url_for('gowreesh', param=User))
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port = 8080)
+    app.run(debug=True, port=8080)
